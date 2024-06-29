4f63fb72d615139e1f87daec04e6d622d1de7222
JaredTeo311 <2717930T@student.gla.ac.uk>
2024-06-29 20:34:57 +0800

    Linted + added explanation to dataprep

--- P3-Assignment-Dimgray-DataPrep.R
+++ P3-Assignment-Dimgray-DataPrep.R
@@ -7,6 +7,17 @@ library(tidyverse)
 library(lubridate)
 library(ggplot2)
 library(RColorBrewer)
+library(knitr)
+
+
+## -----------------------------------------------------------------------------
+#| label: fig-inf
+#| fig-cap: "Heat map of inflationary impact on key itens over 2023,
+#| published by the
+#|   Straits Times."
+#| out-width: 60%
+
+include_graphics("images/price_index.png")
 
 
 ## -----------------------------------------------------------------------------
@@ -17,7 +28,8 @@ library(RColorBrewer)
 
 # Load the dataset
 dataset <- read_csv("M212911.csv", skip = 10)
-dataset <- dataset[1:152,]
+dataset <- dataset[1:152, ]
+dataset
 
 
 ## -----------------------------------------------------------------------------
@@ -26,12 +38,19 @@ dataset <- dataset[1:152,]
 #| warning: false
 
 # Convert relevant columns to numeric
-dataset <- dataset %>%
-  mutate(across(matches("^2019|^2020|^2021|^2022|^2023"), ~ as.numeric(as.character(.))))
+dataset <- dataset |>
+  mutate(across(matches("^2019|^2020|^2021|^2022|^2023"),
+                ~ as.numeric(as.character(.))))
 
-month_data <- dataset %>%
-  filter(`Data Series` %in% c("Food", "Food Excl Food Serving Services", "Clothing & Footwear", "Housing & Utilities", "Household Durables & Services", "Health Care", "Transport", "Communication", "Recreation & Culture", "Education", "Miscellaneous Goods & Services", "All Items")) %>%
+month_data <- dataset |>
+  filter(`Data Series` %in% c("Food", "Food Excl Food Serving Services",
+                              "Clothing & Footwear", "Housing & Utilities",
+                              "Household Durables & Services", "Health Care",
+                              "Transport", "Communication",
+                              "Recreation & Culture", "Education",
+                              "Miscellaneous Goods & Services", "All Items")) |>
   select(`Data Series`, matches("^2019|^2020|^2021|^2022|^2023"))
+month_data
 
 
 ## -----------------------------------------------------------------------------
@@ -40,14 +59,18 @@ month_data <- dataset %>%
 #| warning: false
 
 # Pivot longer for heatmap generation
-long_data <- month_data %>%
-  pivot_longer(cols = matches("^2019|^2020|^2021|^2022|^2023"), 
-               names_to = "Year_Month", values_to = "Percent_Change") %>%
-  separate(Year_Month, into = c("Year", "Month"), sep = " ") %>%
-  mutate(Year = as.integer(Year), 
-         Month = factor(Month, levels = c(month.abb, "Overall_Avg")),  # Add "Overall_Avg" as the 14th month
-         Month_Index = as.numeric(factor(Month, levels = c(month.abb, "Overall_Avg"))),
-         Year_Month_Index = paste(Year, sprintf("%02d", Month_Index), sep = "-"))
+long_data <- month_data |>
+  pivot_longer(cols = matches("^2019|^2020|^2021|^2022|^2023"),
+               names_to = "Year_Month", values_to = "Percent_Change") |>
+  separate(Year_Month, into = c("Year", "Month"), sep = " ") |>
+  mutate(Year = as.integer(Year),
+         # Add "Overall_Avg" as the 14th month
+         Month = factor(Month, levels = c(month.abb, "Overall_Avg")),
+         Month_Index = as.numeric(factor(Month, levels = c(month.abb,
+                                                           "Overall_Avg"))),
+         Year_Month_Index = paste(Year, sprintf("%02d",
+                                                Month_Index), sep = "-"))
+long_data
 
 
 ## -----------------------------------------------------------------------------
@@ -55,12 +78,14 @@ long_data <- month_data %>%
 #| message: false
 #| warning: false
 # Calculate the overall average for each data series from 2019 to 2023
-overall_averages <- long_data %>%
-  filter(Year >= 2019 & Year <= 2023) %>%
-  group_by(`Data Series`) %>%
-  summarize(Percent_Change = mean(Percent_Change, na.rm = TRUE), .groups = 'drop') %>%
-  mutate(Year = 2023, Month = "Overall_Avg", Month_Index = 14, Year_Month_Index = "2023-14")  # Use month index 14 for "Overall_Avg"
-
+overall_averages <- long_data |>
+  filter(Year >= 2019 & Year <= 2023) |>
+  group_by(`Data Series`) |>
+  summarize(Percent_Change = mean(Percent_Change, na.rm = TRUE),
+            .groups = "drop") |>
+  mutate(Year = 2023, Month = "Overall_Avg", Month_Index = 14,
+         Year_Month_Index = "2023-14")  # Use month index 14 for "Overall_Avg"
+overall_averages
 
 
 ## -----------------------------------------------------------------------------
@@ -70,11 +95,14 @@ overall_averages <- long_data %>%
 
 
 # Add a small gap before and after "Overall_Avg"
-gap_data_before_avg <- overall_averages %>%
-  mutate(Month = "Gap", Month_Index = 13, Year_Month_Index = "2023-13", Percent_Change = NA)
+gap_data_before_avg <- overall_averages |>
+  mutate(Month = "Gap", Month_Index = 13, Year_Month_Index = "2023-13",
+         Percent_Change = NA)
 
-gap_data_after_avg <- overall_averages %>%
-  mutate(Month = "Gap", Month_Index = 15, Year_Month_Index = "2023-15", Percent_Change = NA)
+gap_data_after_avg <- overall_averages |>
+  mutate(Month = "Gap", Month_Index = 15, Year_Month_Index = "2023-15",
+         Percent_Change = NA)
+gap_data_after_avg
 
 
 ## -----------------------------------------------------------------------------
@@ -84,18 +112,30 @@ gap_data_after_avg <- overall_averages %>%
 
 
 # Combine the original data with the overall averages and the gaps
-combined_data <- bind_rows(long_data, overall_averages, gap_data_before_avg, gap_data_after_avg) %>%
+combined_data <- bind_rows(long_data, overall_averages,
+                           gap_data_before_avg, gap_data_after_avg) |>
   arrange(Year, Month_Index)
 
 
 color_scale <- scale_fill_gradientn(
-  colors = rev(brewer.pal(9, "RdYlBu")),  # Use the RdYlBu palette with 9 colors in reverse order
-  values = scales::rescale(c(min(combined_data$Percent_Change, na.rm = TRUE), -3, 0, 3, 6, 9, 12, max(combined_data$Percent_Change, na.rm = TRUE))),  # Define breakpoints dynamically
-  limits = c(min(combined_data$Percent_Change, na.rm = TRUE), max(combined_data$Percent_Change, na.rm = TRUE)),  # Dynamic limits
-  breaks = seq(floor(min(combined_data$Percent_Change, na.rm = TRUE)), ceiling(max(combined_data$Percent_Change, na.rm = TRUE)), by = 3),  # Adjust breaks dynamically
-  labels = as.character(seq(floor(min(combined_data$Percent_Change, na.rm = TRUE)), ceiling(max(combined_data$Percent_Change, na.rm = TRUE)), by = 3)),  # Adjust labels dynamically
-  na.value = "white"  # Set color for NA values
-  )
+  # Use the RdYlBu palette with 9 colors in reverse order
+  colors = rev(brewer.pal(9, "RdYlBu")),
+  values = scales::rescale(c(min(combined_data$Percent_Change, na.rm = TRUE),
+                             -3, 0, 3, 6, 9, 12,
+                             max(combined_data$Percent_Change, na.rm = TRUE))),
+  # Define breakpoints dynamically
+  limits = c(min(combined_data$Percent_Change, na.rm = TRUE),
+             max(combined_data$Percent_Change, na.rm = TRUE)),
+  breaks = seq(floor(min(combined_data$Percent_Change, na.rm = TRUE)),
+               ceiling(max(combined_data$Percent_Change, na.rm = TRUE)),
+               by = 3),  # Adjust breaks dynamically
+  labels = as.character(seq(floor(min(combined_data$Percent_Change,
+                                      na.rm = TRUE)),
+                            ceiling(max(combined_data$Percent_Change,
+                                        na.rm = TRUE)),
+                            by = 3)),  # Adjust labels dynamically
+  na.value = "white" # Set color for NA values
+)
 
 
 ## -----------------------------------------------------------------------------
@@ -105,14 +145,17 @@ color_scale <- scale_fill_gradientn(
 
 
 # Adjust the data series names for better readability
-combined_data <- combined_data %>%
+combined_data <- combined_data |>
   mutate(`Data Series` = case_when(
-    `Data Series` == "Miscellaneous Goods & Services" ~ "Miscellaneous\nGoods & Services",
-    `Data Series` == "Household Durables & Services" ~ "Household Durables\n& Services",
-    `Data Series` == "Food Excl Food Serving Services" ~ "Food Excl Food\nServing Services",
+    `Data Series` ==
+      "Miscellaneous Goods & Services" ~ "Miscellaneous\nGoods & Services",
+    `Data Series` ==
+      "Household Durables & Services" ~ "Household Durables\n& Services",
+    `Data Series` ==
+      "Food Excl Food Serving Services" ~ "Food Excl Food\nServing Services",
     TRUE ~ as.character(`Data Series`)
   ))
-
+combined_data
 
 
 ## -----------------------------------------------------------------------------
@@ -121,25 +164,28 @@ combined_data <- combined_data %>%
 #| warning: false
 
 
-# Generate the x-axis breaks and labels to include January, July, and "Overall_Avg" for each year
-x_breaks <- combined_data %>%
-  filter(Month %in% c("Jan", "Jul", "Overall_Avg", "Gap")) %>%
-  mutate(Year_Month_Index = paste(Year, sprintf("%02d", Month_Index), sep = "-")) %>%
-  distinct(Year_Month_Index, .keep_all = TRUE) %>%
-  arrange(Year, Month_Index) %>%
+# Generate the x-axis breaks and labels to include January, July, and
+# "Overall_Avg" for each year
+x_breaks <- combined_data |>
+  filter(Month %in% c("Jan", "Jul", "Overall_Avg", "Gap")) |>
+  mutate(Year_Month_Index = paste(Year, sprintf("%02d", Month_Index),
+                                  sep = "-")) |>
+  distinct(Year_Month_Index, .keep_all = TRUE) |>
+  arrange(Year, Month_Index) |>
   pull(Year_Month_Index)
 
-x_labels <- combined_data %>%
-  filter(Month %in% c("Jan", "Jul", "Overall_Avg", "Gap")) %>%
-  mutate(Year_Month_Index = paste(Year, sprintf("%02d", Month_Index), sep = "-")) %>%
-  distinct(Year_Month_Index, .keep_all = TRUE) %>%
-  arrange(Year, Month_Index) %>%
+x_labels <- combined_data |>
+  filter(Month %in% c("Jan", "Jul", "Overall_Avg", "Gap")) |>
+  mutate(Year_Month_Index = paste(Year, sprintf("%02d", Month_Index),
+                                  sep = "-")) |>
+  distinct(Year_Month_Index, .keep_all = TRUE) |>
+  arrange(Year, Month_Index) |>
   mutate(Label = case_when(
     Month == "Overall_Avg" ~ paste("Overall Avg"),
     Month == "Gap" ~ "",
     Month == "Jan" ~ paste("Jan", Year),
     Month == "Jul" ~ paste("Jul", Year),
     TRUE ~ month.abb[Month_Index]
-  )) %>%
+  )) |>
   pull(Label)
 
